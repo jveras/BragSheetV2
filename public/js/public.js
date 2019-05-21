20feee58c9afe51fc7a0bab1304bd3b31a3d6032
professionaljared <jknight@aoiths.org>
2019-05-21 16:19:09 -0400

    removed a few comments

--- public/js/public.js
+++ public/js/public.js
@@ -1,45 +1,7 @@
 //Register JavaScipt
 $(".main").hide();
 $(".logo").hide();
-// var logo = ["aoit.png", "urban.png", "brooklyn.png", "CH.png", "multi.png"];
 
-// $("#selectSchool").on("change", function() {
-//     console.log(this.value);
-//     if (this.value == 0) {
-//         $(".main").hide();
-//         $(".logo").hide();
-//     } else if (this.value == 1) {
-//         $(".selectSchoolCounselors").val(this.value);
-//         $(".main").show();
-//         $(".logo").attr("src", "/img/" + logo[this.value - 1]);
-//         $(".logo").css("height", "200px", );
-//         $(".logo").show();
-//     } else if (this.value == 2) {
-//         $(".selectSchoolCounselors").val(this.value);
-//         $(".main").show();
-//         $(".logo").attr("src", "/img/" + logo[this.value - 1]);
-//         $(".logo").css("height", "200px", );
-//         $(".logo").show();
-//     } else if (this.value == 3) {
-//         $(".selectSchoolCounselors").val(this.value);
-//         $(".main").show();
-//         $(".logo").attr("src", "/img/" + logo[this.value - 1]);
-//         $(".logo").css("height", "200px", );
-//         $(".logo").show();
-//     } else if (this.value == 4) {
-//         $(".selectSchoolCounselors").val(this.value);
-//         $(".main").show();
-//         $(".logo").attr("src", "/img/" + logo[this.value - 1]);
-//         $(".logo").css("height", "200px", );
-//         $(".logo").show();
-//     } else if (this.value == 5) {
-//         $(".selectSchoolCounselors").val(this.value);
-//         $(".main").show();
-//         $(".logo").attr("src", "/img/" + logo[this.value - 1]);
-//         $(".logo").css("height", "200px", );
-//         $(".logo").show();
-//     }
-// });
 
 
 function schoolSelectionFunction (){
